464b5694e6eb86d68befe9803b52d2a8b94d3df3
merry <1262411798@qq.com>
2023-07-10 11:09:59 +0800

    home/download

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -4,9 +4,10 @@ export default navbar([
   "/quick-start",
   "/guide/",
   "/changelog",
-  {
-    text: "下载地址",
-    icon: "book",
-    link: "https://www.baidu.com",
-  },
+  "/download",
+  // {
+  //   text: "下载地址",
+  //   icon: "book",
+  //   link: "https://mp-ced0d90a-75a8-498b-adb7-2fa8c4dd302f.cdn.bspapp.com/cloudstorage/merrymonitor.zip",
+  // },
 ]);
---
+++ src/download.md
@@ -0,0 +1,11 @@
+---
+title: 下载地址
+icon: download
+---
+
+::: tip 下载安装方式
+
+1. [merrymonitor.zip](https://mp-ced0d90a-75a8-498b-adb7-2fa8c4dd302f.cdn.bspapp.com/cloudstorage/merrymonitor.zip) 通过压缩包解压安装。
+2. [merrymonitor-setup.exe](https://mp-ced0d90a-75a8-498b-adb7-2fa8c4dd302f.cdn.bspapp.com/cloudstorage/merrymonitor-setup.exe) 通过安装程序安装。
+
+:::
