04d299168e54b1a728d1099f0857528237bd4f12
merry <1262411798@qq.com>
2023-07-10 10:31:10 +0800

    pages

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import theme from "./theme.js";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
-  base: "/", 
+  base: "/merry-monitor/", 
 
   lang: "zh-CN",
   title: "Merry Monitor",
